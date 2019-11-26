8b254b80bc0a3403e2350a195bec3f52cea38dcc
Jan Justyn Dela Cruz <blitzersonicz@gmail.com>
2019-11-26 13:16:07 -0500

    Initial Commit

---
+++ SpaceX/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  SpaceX
+//
+//  Created by Jan Justyn Dela Cruz on 11/26/19.
+//  Copyright © 2019 Jan Justyn Dela Cruz. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
